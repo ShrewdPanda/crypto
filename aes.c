068bd67add09ed138ad3598f96b97db64ee474f6
Carl Weathers <modicum@protonmail.com>
2018-12-14 07:09:24 -0500

    14 Dec 2018 07:00

---
+++ aes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define NUM_ROWS 4
+#define NUM_COLS 4
+
+
+int CatVar(int m, int n)
+{
+	int pow = 10;
+	while (n >= pow)
+		pow *= 10;
+	return m * pow + n;
+}
+
+void ShiftRows(int InBlock[NUM_ROWS][NUM_COLS])
+{
+	
+}
+
+void CreateArray(int InBlock[NUM_ROWS][NUM_COLS])
+{	
+	int m, n;
+
+	for (m = 0; m < NUM_ROWS; m++)
+	{
+		for (n = 0; n < NUM_COLS; n++)
+		{	
+			InBlock[m][n] = CatVar(m, n);
+			printf("%d ", InBlock[m][n]);
+		}
+		printf("\n");
+	}
+
+	ShiftRows(InBlock);
+}
+
+
+int main(void)
+{
+	int InBlock[NUM_ROWS][NUM_COLS];
+	
+	CreateArray(InBlock);
+}
